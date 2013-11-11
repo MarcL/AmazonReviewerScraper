606e36a3b4c6b00ca5ab3a7ed77b5a2f239a26ce
Marc Littlemore <marc.littlemore@bbc.co.uk>
2013-11-11 13:52:47 +0000

    Now parses through multiple pages

--- amazon.rb
+++ amazon.rb
@@ -5,11 +5,14 @@ class AmazonReviewerParser
 
 	def initialize(baseUrl)
 		@baseUrl = baseUrl
-		@maxPages = 3
-		@numPages = 0
+		@maxPages = 1
+		@numPages = 1
+
+		@reviewPageUrl = @baseUrl + "/review/top-reviewers?page="
 	end
 
-	def ParseTopReviewersPage(pageUrl)
+	def ParseTopReviewersPage(pageNum)
+		pageUrl = @reviewPageUrl + @numPages.to_s
 		data = Nokogiri::HTML(open(pageUrl))
 
 		# Get links
@@ -18,24 +21,27 @@ class AmazonReviewerParser
 		# Remove any nils and make them unique (or we get multiple links to the same page)
 		links = links.compact!.uniq
 
-		# links.each{ |link| ParseReviewerPage(@baseUrl + link)}
+		# Follow each reviewer link and parse their data
+		links.each{ |link| ParseReviewerPage(@baseUrl + link)}
 
-		# Next page if we have one
-		nextPage = data.xpath("/html/body/table/tbody/tr/td[1]/table[3]/tbody/tr/td[2]/div/span/a[3]/@href")
-		puts nextPage
+		# TODO: Flush a page at a time to a CSV file or similar
+
 		@numPages += 1
-		if (!nextPage.nil? and @numPages < @maxPages)
+
+		# Reviewers page URL: /review/top-reviewers?page=<pagenum>
+		if (@numPages <= @maxPages)
+			ParseTopReviewersPage(@numPages)
 		end
 
 	end
 
+	private
 	def ParseReviewerPage(pageUrl)
 
 		puts pageUrl
 		data = Nokogiri::HTML(open(pageUrl))
 
 		# Name
-		# //*[@id="profileHeader"]/table/tbody/tr/td/table/tbody/tr/td/h1/span
 		name = data.xpath("//*/h1/span").text
 		puts "Name: " + name
 
@@ -68,8 +74,6 @@ class AmazonReviewerParser
 		puts "Own words: " + ownWords
 
 		# Interests
-
-		# //*[@id="interestsTags"]/div/div/div/div/div[1]
 		# Check the pop-up first as it's the longer version
 		interests = data.xpath("//*[@id=\"textPop_interests\"]").text
 		if (interests.nil? or interests.empty?)
@@ -81,26 +85,4 @@ end
 
 
 parser = AmazonReviewerParser.new("http://www.amazon.co.uk")
-# parser.ParseReviewerPage("http://www.amazon.co.uk/gp/pdp/profile/A1E0K9B6TW9MEY/ref=cm_cr_tr_tbl_1_name")
-# parser.ParseReviewerPage("http://www.amazon.co.uk/gp/pdp/profile/A2N6VMZN3XIIYK/ref=cm_cr_tr_tbl_2_name")
-# parser.ParseReviewerPage("http://www.amazon.co.uk/gp/pdp/profile/A1JUKS0DSO2XZG/ref=cm_cr_tr_tbl_3_name")
-# parser.ParseReviewerPage("http://www.amazon.co.uk/gp/pdp/profile/A68CUQ90V1K1Y/ref=cm_cr_tr_tbl_5_name")
-# parser.ParseReviewerPage("http://www.amazon.co.uk/gp/pdp/profile/A1RD1LL5E5JZOT/ref=cm_cr_tr_tbl_6_name")
-# parser.ParseReviewerPage("http://www.amazon.co.uk/gp/pdp/profile/A2KG24N8MMUNFI/ref=cm_cr_tr_tbl_7_name")
-parser.ParseTopReviewersPage("http://www.amazon.co.uk/reviews/top-reviewers")
-
-
-# url = "http://www.amazon.co.uk/reviews/top-reviewers"
-# # url = "http://www.amazon.co.uk/Mice-Genius-Study-Guide-ebook/dp/B00AJ3XEWO"
-# # url = "http://www.amazon.co.uk/Revision-Literature-Annoying-Brothers-ebook/dp/B00CQYOYEG/ref=pd_sim_sbs_kinc_2"
-
-# data = Nokogiri::HTML(open(url))
-
-# # Regular expression to get number after a hash: #([1-9](?:\d{0,2})(?:,\d{3})*(?:\.\d*[1-9])?|0?\.\d*[1-9]|0)
-# reviewers = data.at_css('tr#reviewer1').text
-# puts reviewers
-
-# # re = /#([1-9](?:\d{0,2})(?:,\d{3})*(?:\.\d*[1-9])?|0?\.\d*[1-9]|0)/
-# # matches = salesRank.match re
-
-# # puts matches
+parser.ParseTopReviewersPage(1)
