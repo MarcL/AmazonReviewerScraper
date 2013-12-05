cc17ecf92dbdcedf48715e2ced8343bc2d99f147
Marc Littlemore <marc.littlemore@gmail.com>
2013-12-05 16:21:18 +0000

    Testing example with another ASIN.

--- examples/reviews-csv.rb
+++ examples/reviews-csv.rb
@@ -2,7 +2,7 @@ require './lib/AmazonReviewerScraper'
 require 'csv'
 
 scraper = AmazonReviewerScraper.new(:baseUrl => "http://www.amazon.co.uk", :maxPages => 10, :followNextLink => true)
-scraper.ParseASINReviews("B00GJTV0J6")
+scraper.ParseASINReviews("B00DTWCHH0")
 reviews = scraper.reviews
 
 # Write all reviews to a file
