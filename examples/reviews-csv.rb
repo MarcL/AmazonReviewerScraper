c01fa15429802593dd44473060d8b597b42d95bc
Marc Littlemore <marc.littlemore@gmail.com>
2013-12-06 09:49:52 +0000

    Changed reviews-csv example to pass ASIN via command line.

--- examples/reviews-csv.rb
+++ examples/reviews-csv.rb
@@ -1,12 +1,31 @@
 require './lib/AmazonReviewerScraper'
 require 'csv'
 
+def usage()
+	puts "Usage: ruby reviews-csv.rb -asin <ASIN>"
+end
+
+# Get arguments
+case ARGV[0]
+when "-asin"
+	if (ARGV[1].nil?)
+		usage()
+		exit 1
+	else
+		asin = ARGV[1]
+	end
+else
+	usage()
+	exit 1	
+end
+
+
 scraper = AmazonReviewerScraper.new(:baseUrl => "http://www.amazon.co.uk", :maxPages => 10, :followNextLink => true)
-scraper.ParseASINReviews("B00DTWCHH0")
+scraper.ParseASINReviews(asin)
 reviews = scraper.reviews
 
 # Write all reviews to a file
-CSV.open("all-reviews.csv", "w") do |csv|
+CSV.open("#{asin}-reviews.csv", "w") do |csv|
 	csv << ["rank", "name", "numStars", "email", "website", "review"]
 	reviews.each { |review|
 		reviewer = review["reviewer"]
